91f34bffe8112acd8ed4cdf52ece107beae6fc9e
RenatDoroshenko <renat.doroshenko@sitecore.com>
2019-05-13 14:35:04 +0300

    Commit Patch

---
+++ src/Sitecore.Support.93603/Shell/Framework/Commands/DeleteChildrenPatched.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Framework.Commands;
+using System.Linq;
+
+namespace Sitecore.Support.Shell.Framework.Commands
+{
+  public class DeleteChildrenPatched : DeleteChildren
+  {
+    public override void Execute(CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      Items.DeleteChildren(context.Items);
+    }
+
+    public override CommandState QueryState(CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      return ((context.Items.Sum<Item>(item => Items.GetDeleteableChildren(item).Count) != 0) ? CommandState.Enabled : CommandState.Disabled);
+    }
+
+  }
+}
